e5823b4722f8df6bd30633d8248de6286f4f322e
Iury Nogueira <ikdn@ic.ufal.br>
2024-03-09 05:48:12 -0300

    ponto_segurança

--- Huffman/Arquivos_code/headers/descomprimir.h
+++ Huffman/Arquivos_code/headers/descomprimir.h
@@ -1,7 +1,22 @@
-#ifndef HUFFMAN_DECOMPRESS_H
-#define HUFFMAN_DECOMPRESS_H
+#ifndef DESCOMPRIMIR_H
+#define DESCOMPRIMIR_H
 
 #include "heap.h"
 #include "hash.h"
 #include "arvore_huffman.h"
 
+int is_bit_set(u_char c, int i);
+
+int qual_tamanho_lixo(FILE *input);
+
+short qual_tamanho_arvore(FILE *input);
+
+TREE* qual_e_arvore(FILE* input, TREE* tree);
+
+void decomprimir_one_ascii_file(FILE* input, FILE* output, TREE* tree, int trash_size);
+
+void descomprimir_arquivo(FILE* input, FILE* output, TREE* tree, int trash_size);
+
+void descomprimir(FILE *input, FILE *output);
+
+#endif //DESCOMPRIMIR_H
---
+++ Huffman/Arquivos_code/headers/func_extras.h
@@ -0,0 +1,21 @@
+#ifndef FUNC_EXTRAS_H
+#define FUNC_EXTRAS_H
+
+
+#include <stdio.h>
+
+#include "heap.h"
+#include "hash.h"
+#include "arvore_huffman.h"
+
+void criacao_fila_prioridade(lli frequencia[], HEAP *heap);
+
+lli *contador_de_frequencia(FILE *file);
+
+void mallocou(void *mem);
+
+HEAP *montar_heap(FILE *arquivo_entrada);
+
+char* concat(char *s1, char *s2);
+
+#endif /* FUNC_EXTRAS_H */
